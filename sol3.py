16056c5f4584ea0a18301d37cea5b90a586b0f8c
toluenesama <toludaniel.ojo@gmail.com>
2025-03-22 14:29:11 +0100

    final commit

--- sol3.py
+++ sol3.py
@@ -1,23 +1,19 @@
 import re
 
-format_finding = r"mul\(\d+,\d+\)"
-format_do = r"do(?:\(\))"
-format_dont = r"don't(?:\(\))"
+format_finding = r"mul\(\d+,\d+\)" #format of string to be found
 
-mul_all = []
-mul_dont = []
-mul_do = []
-mul_doo = []
-mul_dontdo = []
-do = []
-line_count = 0
-k = ""
 
+mul_all = [] #initialize the list of mul functions found
+
+k = "" #Initialize the whole text
+
+# get the whole text to one string
 with open("day3.txt","r") as file:
     for line in file:
         line.rstrip('\n')
         k += line
 print(len(k))
+
 
 with open("day3.txt","r") as file:
     for line in file:
@@ -26,16 +22,20 @@ with open("day3.txt","r") as file:
 
 
 print(type(k))
-tetz = "ihasgdhaijdon't()ghgahsdo()diasddon't()hdo()gasvdo()isjgadon't()dfhasi"
-
+# split by dont()
 tests_donts = k.split("don't()")
+
+# remove the first set since they are not affected
 unaffected = k.split("don't()").pop(0)
+# making sure theyre in a singletext
 unnafected_text = "".join(unaffected)
 tests_donts.pop(0)
 tests_donts_dos = []
+# spliting the whole dont statements by dos
 for tests in tests_donts:
     tests_donts_dos.append(tests.split("do()"))
 
+# delete the first since they are not affected yet
 for kunta in tests_donts_dos:
     kunta.pop(0)
 
@@ -47,10 +47,8 @@ for hui in tests_donts_dos:
 
 
 all = unaffected + tests_donts_dos_texts
-print(len(all))
 
 mul_lists = re.findall(format_finding,all)
-print(len(unnafected_text))
 
 sum = 0
 sum_all = 0
@@ -70,7 +68,6 @@ for i in mul_all:
 for i in mul_lists:
     k = i[3:][1:]
     m = k[:len(k)-1].split(",")
-    print(m)
     for i in range(len(m) - 1):
         sum += int(m[i]) * int(m[i +1])
 print(sum)
